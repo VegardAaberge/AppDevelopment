c86e54371104bd59f4b0fac9e41cf60f561646b2
Vegard Aaberge <vegard@unizite.com>
2022-03-30 18:13:32 +0800

    Create a music knob that can be used to play music
    Some changes compared to tutorial
    - Used pointerInput
    - Play music when the knob is turned

--- ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -3,40 +3,18 @@ package com.example.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            
+            MusicKnob().Setup(context = this)
         }
     }
 }
 
-@@Composable
-fun MusicKnob(
-    modifier: Modifier = Modifier,
-    limitingAngle: Float = 25f,
-    onValueChange: (Float) -> Unit
-) {
-    var rotation by remember{
-        mutableStateOf(limitingAngle)
-    }
-    var touchx by remember {
-        mutableStateOf(0)
-    }
-    var touchy by remember {
-        mutableStateOf(0)
-    }
-    var centerX by remember {
-        mutableStateOf(0)
-    }
 
-}
 
 
 
