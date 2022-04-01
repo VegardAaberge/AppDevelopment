69bb7498340a5dcf44236b60ea4b006c613bdc4c
Vegard Aaberge <vegard@unizite.com>
2022-04-01 23:38:02 +0800

    Create ScreenSize that look different with different width

---
+++ ComposeCourse/app/src/main/java/com/example/composecourse/ScreenSizes.kt
@@ -0,0 +1,80 @@
+package com.example.composecourse
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composecourse.models.WindowInfo
+import com.example.composecourse.models.rememberWindowInfo
+
+@Composable
+fun SetupScreenSizes() {
+    val windowInfo = rememberWindowInfo()
+
+    if(windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact){
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ){
+            items(10){
+                Text1(it)
+            }
+            items(10){
+                Text2(it)
+            }
+        }
+    }else {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            LazyColumn(
+                modifier = Modifier.weight(1f)
+            ){
+                items(10){
+                    Text1(it)
+                }
+            }
+            LazyColumn(
+                modifier = Modifier.weight(1f)
+            ){
+                items(10){
+                    Text2(it)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Text1(itemIndex : Int) {
+    Text(
+        text = "Items ${itemIndex}",
+        fontSize = 25.sp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Cyan)
+            .padding(16.dp)
+
+    )
+}
+
+@Composable
+fun Text2(itemIndex : Int) {
+    Text(
+        text = "Items ${itemIndex}",
+        fontSize = 25.sp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Green)
+            .padding(16.dp)
+
+    )
+}
+
