0c9e567b16082353705a3e7b28cba9accc1ba7a1
VegardAaberge <vegard.aaberge@gmail.com>
2022-04-01 17:47:00 +0800

    Create a extended custom theme with a spacing property

--- ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
+++ ComposeCourse/app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            RequestPermissionsAndDisplay()
+            ComposeCourseTheme{
+                
+            }
         }
     }
 }
