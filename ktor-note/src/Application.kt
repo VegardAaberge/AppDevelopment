4bac0081b16bdc861c4c7d80b3e7951ae1a0f22b
VegardAaberge <vegard.aaberge@gmail.com>
2022-04-14 15:50:51 +0800

    Create a route to register users

--- ktor-note/src/Application.kt
+++ ktor-note/src/Application.kt
@@ -1,9 +1,16 @@
 package com.androiddevs
 
+import com.androiddevs.data.collections.User
+import com.androiddevs.data.registerUser
+import com.androiddevs.routes.registerRoute
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.routing.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -12,7 +19,9 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     install(DefaultHeaders)         // Extra info for all our responses
     install(CallLogging)            // Log all our http requests and the responses
-    install(Routing)                // Can define URL endpoints. REST API
+    install(Routing) {   // Can define URL endpoints. REST API
+        registerRoute()
+    }
     install(ContentNegotiation) {   // Make sure which type the server can respond with
         gson {
             setPrettyPrinting()
---
+++ ktor-note/src/routes/RegisterRoutes.kt
@@ -0,0 +1,36 @@
+package com.androiddevs.routes
+
+import com.androiddevs.data.checkIfUserExists
+import com.androiddevs.data.collections.User
+import com.androiddevs.data.registerUser
+import com.androiddevs.data.requests.AccountRequest
+import com.androiddevs.data.responses.SimpleResponse
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Route.registerRoute(){
+    route("/register"){
+        post {
+            val request = try {
+                call.receive<AccountRequest>()
+            }catch (e: ContentTransformationException){
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+            val userExist = checkIfUserExists(request.email)
+            if(!userExist){
+                val newUser = User(request.email, request.password)
+                if(registerUser(newUser)){
+                    call.respond(HttpStatusCode.OK, SimpleResponse(true, "Successfully created an account"))
+                }else{
+                    call.respond(HttpStatusCode.OK, SimpleResponse(false, "An unknown error occurred"))
+                }
+            }else{
+                call.respond(HttpStatusCode.OK, SimpleResponse(false, "A user with that email already exist"))
+            }
+        }
+    }
+}
