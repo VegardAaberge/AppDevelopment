ccf2918ef086daccd7df154f958b899e1cac7852
VegardAaberge <vegard.aaberge@gmail.com>
2022-04-24 20:22:46 +0800

    Use https instead of http

--- Apps/ktor-note-app/app/src/main/java/com/androiddevs/ktornoteapp/di/AppModule.kt
+++ Apps/ktor-note-app/app/src/main/java/com/androiddevs/ktornoteapp/di/AppModule.kt
@@ -19,7 +19,13 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import javax.inject.Singleton
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -42,9 +48,10 @@ class AppModule {
     @Singleton
     @Provides
     fun provideNoteApi(
+        okHttpClient: OkHttpClient.Builder,
         basicAuthInterceptor: BasicAuthInterceptor
     ) : NoteApi {
-        val client = OkHttpClient.Builder()
+        val client = okHttpClient
             .addInterceptor(basicAuthInterceptor)
             .build()
         return Retrofit.Builder()
@@ -53,6 +60,28 @@ class AppModule {
             .client(client)
             .build()
             .create(NoteApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient.Builder {
+        val trustAllCertificates: Array<TrustManager> = arrayOf(
+            object : X509TrustManager {
+                override fun checkClientTrusted(p0: Array<out X509Certificate>?, p1: String?) {}
+
+                override fun checkServerTrusted(p0: Array<out X509Certificate>?, p1: String?) {}
+
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    return arrayOf()
+                }
+            }
+        )
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCertificates, SecureRandom())
+
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustAllCertificates[0] as X509TrustManager)
+            .hostnameVerifier(HostnameVerifier { _, _ -> true })
     }
 
     @Singleton
