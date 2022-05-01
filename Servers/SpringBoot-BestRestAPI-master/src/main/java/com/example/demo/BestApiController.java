848829d77380fec4e83eb4d958dcfc64ea30398c
VegardAaberge <vegard.aaberge@gmail.com>
2022-05-01 19:37:12 +0800

    Use the initializer to setup the project with the right dependencies

--- Servers/SpringBoot-BestRestAPI-master/src/main/java/com/example/demo/BestApiController.java
+++ Servers/SpringBoot-BestRestAPI-master/src/main/java/com/example/demo/BestApiController.java
@@ -1,12 +1,13 @@
-package com.eduonix.BestRestApiEver.controller;
+package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BestApiController {
 	
-	@RequestMapping("/")
+	@GetMapping
 	public String bestApi() {
 		return "This is the best api you've ever seen";
 	}
--- Servers/SpringBoot-BestRestAPI-master/src/main/java/com/example/demo/BestRestApiEverApplication.java
+++ Servers/SpringBoot-BestRestAPI-master/src/main/java/com/example/demo/BestRestApiEverApplication.java
@@ -1,4 +1,4 @@
-package com.eduonix.BestRestApiEver;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class BestRestApiEverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BestRestApiEverApplication.class, args);
 	}
+
 }
