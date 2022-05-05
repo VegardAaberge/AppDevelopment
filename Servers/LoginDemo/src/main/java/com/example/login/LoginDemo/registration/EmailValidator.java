c810628c22b632aec88ee1fe0c38b0ea5f668477
VegardAaberge <vegard.aaberge@gmail.com>
2022-05-05 12:18:02 +0800

    Create the api to sign up users

---
+++ Servers/LoginDemo/src/main/java/com/example/login/LoginDemo/registration/EmailValidator.java
@@ -0,0 +1,16 @@
+package com.example.login.LoginDemo.registration;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+
+@Service
+public class EmailValidator implements Predicate<String> {
+
+    @Override
+    public boolean test(String s) {
+
+        // TODO Regex validate email
+        return true;
+    }
+}
--- Servers/LoginDemo/src/main/java/com/example/login/LoginDemo/registration/RegistrationService.java
+++ Servers/LoginDemo/src/main/java/com/example/login/LoginDemo/registration/RegistrationService.java
@@ -1,11 +1,33 @@
 package com.example.login.LoginDemo.registration;
 
+import com.example.login.LoginDemo.appuser.AppUser;
+import com.example.login.LoginDemo.appuser.AppUserRole;
+import com.example.login.LoginDemo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
-    public String register(RegistrationRequest registrationRequest) {
-        return "It works";
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request) {
+
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
