c7177c4f8ca5d1b35c72f4e174ea1841ba405134
VegardAaberge <vegard.aaberge@gmail.com>
2022-05-13 10:19:08 +0800

    Create an Authorization Filter and add it to http

--- Servers/userservice/src/main/java/com/example/userservice/security/SecurityConfig.java
+++ Servers/userservice/src/main/java/com/example/userservice/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.userservice.security;
 
 import com.example.userservice.filter.CustomAuthenticationFilter;
+import com.example.userservice.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -40,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(GET, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
